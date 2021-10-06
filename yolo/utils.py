69008354eb7498521b5acf1e36592b9b3367907c
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-10-06 12:01:08 +0700

    Move file and implement resize bbox

--- yolo/utils.py
+++ yolo/utils.py
@@ -1,16 +1,20 @@
 import tensorflow as tf
 import numpy as np
-from typing import List
+import cv2
+
+from typing import Tuple, List, Union, Iterable
+from nptyping import NDArray
+
 
 @tf.function(experimental_follow_type_hints=True)
 def calculate_iou(bbox1: tf.Tensor, bbox2: tf.Tensor) -> tf.Tensor:
     """Calculate intersection over union between bounding boxes.
     bbox shape = (BATCH_SIZE, S, S, 4)
     4 -> x, y, w, h
-    
+
     Args:
-        bbox1 (tf.Tensor) 
-        bbox2 (tf.Tensor) 
+        bbox1 (tf.Tensor)
+        bbox2 (tf.Tensor)
 
     Returns:
         tf.Tensor: (BATCH_SIZE, S, S)
@@ -38,9 +42,15 @@ def calculate_iou(bbox1: tf.Tensor, bbox2: tf.Tensor) -> tf.Tensor:
 
     return area_inter / (area_bbox1 + area_bbox2 - area_inter + 1e-6)
 
+
 def argmax_to_max(arr, argmax, axis):
-    # taken from https://stackoverflow.com/questions/46103044/index-n-dimensional-array-with-n-1-d-array
-    """argmax_to_max(arr, arr.argmax(axis), axis) == arr.max(axis)"""
+    """
+    Index n dimensional array with n-1 dimensional array. Dimension of the result will be reduced
+    by one.
+
+    argmax_to_max(arr, arr.argmax(axis), axis) == arr.max(axis)
+    taken from https://stackoverflow.com/questions/46103044/index-n-dimensional-array-with-n-1-d-array
+    """
     new_shape = list(arr.shape)
     del new_shape[axis]
 
@@ -48,6 +58,25 @@ def argmax_to_max(arr, argmax, axis):
     grid.insert(axis, argmax)
 
     return arr[tuple(grid)]
+
+
+def resize_image_and_bbox(img_arr: NDArray, bbox_arr: Iterable[Iterable[int]], new_dim: Iterable) -> Tuple[NDArray, Iterable]:
+    """Resize image alongside its bounding boxes
+
+    Args:
+        img_arr (NDArray): 
+        bbox_arr (Iterable[Iterable[int]]): array of x, y
+        new_dim (NDArray): widht, height
+
+    Returns:
+        Tuple[NDArray, Iterable]: resized img_arr and bounding boxes
+    """
+    ori_shape = img_arr.shape
+    mat = np.array([[new_dim[0] / ori_shape[1], 0], [0, new_dim[1] / ori_shape[0]]], dtype=np.float32)
+    new_bbox = list(map(lambda x: np.matmul(mat, x).astype(int), bbox_arr))
+    new_img_arr = cv2.resize(img_arr, new_dim)
+    return new_img_arr, new_bbox
+
 
 if __name__ == '__main__':
     assert calculate_iou(tf.constant([[.25, .25, .5, .5]]), tf.constant([[.75, .75, .5, .5]])).numpy() == 0
--- yolo/v1/losses.py
+++ yolo/v1/losses.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-from yolo.v1.utils import calculate_iou, argmax_to_max
+from yolo.utils import calculate_iou, argmax_to_max
 
 
 class YoloV1Loss():
